6a8efe45c445c7551f6ef3f1c3fb219694ee27c7
Unknown <galih.setyawan24@mail.ugm.ac.id>
2019-04-28 18:52:43 +0700

    basic

---
+++ check_FCS/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace check_FCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFCS = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(63, 73);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(211, 20);
+            this.txtWord.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Word";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(63, 99);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(211, 20);
+            this.txtLength.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Length";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFCS
+            // 
+            this.txtFCS.Location = new System.Drawing.Point(63, 125);
+            this.txtFCS.Name = "txtFCS";
+            this.txtFCS.Size = new System.Drawing.Size(211, 20);
+            this.txtFCS.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "FCS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 210);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFCS);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtWord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFCS;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ check_FCS/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace check_FCS
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtLength.Text = Convert.ToString(txtWord.TextLength);
+
+            //int n = txtWord.TextLength;
+            //int i = 0;
+            //int a = 0;
+            //while (i < n)
+            //{
+            //    a = (byte)Convert.ToChar(a) ^ (byte)Convert.ToChar(txtWord.Text.Substring(i, 1));
+            //    i++;
+            //}
+            //txtFCS.Text = string.Format("{0:x}", a).ToUpper();
+            txtFCS.Text = string2FCS(txtWord.Text);
+        }
+        public string string2FCS(string wordText)
+        {
+            int n = wordText.Length;
+            int i = 0;
+            int a = 0;
+            while (i < n)
+            {
+                a = (byte)Convert.ToChar(a) ^ (byte)Convert.ToChar(wordText.Substring(i, 1));
+                i++;
+            }
+            
+            return string.Format("{0:x}", a).ToUpper();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
